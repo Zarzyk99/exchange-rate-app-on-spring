4db16afa0dae0759451566ff73a497ba58668f1d
zarzyk <zarzyk99@gmail.com>
2023-03-12 15:08:03 +0100

    removed unnesesery comment

--- src/main/java/pl/kurs/exchangerateapponspring/config/AppConfig.java
+++ src/main/java/pl/kurs/exchangerateapponspring/config/AppConfig.java
@@ -1,7 +1,6 @@
 package pl.kurs.exchangerateapponspring.config;
 
 public interface AppConfig {
-    //https://api.fastforex.io/fetch-all?from=EUR&api_key=51c632107c-9a87ee79b8-rje1zh
 
     String EXCHANGERATESAPI_PAGE = "https://api.fastforex.io";
     String PAGE_ENDPOINT = "/fetch-all?from=";
